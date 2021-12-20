834423c24a5abdacb8aa7dfb837b76005a13b294
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2021-12-20 12:29:58 -0700

    start
    for toh rpg

---
+++ pronouns.lua
@@ -0,0 +1,11 @@
+local t = {}
+
+--[=[
+[link](https://github.com/desecaw13/Owl-House-RPG/blob/10665728226e1fde0164ee9c9e5f1bb3cf642af3/design%20docs/archy.txt#L25)
+pronouns- list from enum; they/them, she/her, he/him, it/its, none(name used), neo/custom ([8] long string)
+
+This will be a read-only table of tables: builtins, an option for none, and systems (one for making and using neos, and another for mixing multiple pronoun usage).
+
+]=]
+
+return t
